66e58aa45353737a00936974495a2e0cb3ea8071
Tae Kim <taekim.dev@gmail.com>
2023-03-17 02:22:42 +0800

    Edited Chat component to handle inputs

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,12 +4,34 @@ import UserInput from '../UserInput/UserInput';
 import './Chat.css';
 
 function Chat() {
-  const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState([
+        {
+          type: 'bot',
+          text: 'Welcome to Weather Chat! Type the name of a city to get the weather information.',
+        },
+      ]);
+    
+      const handleUserInput = (city) => {
+        // Add the user's message to the messages array
+        setMessages([...messages, { type: 'user', text: city }]);
+      
+        // Fetch weather data for the city
+        fetchWeatherData(city);
+      };
+      
+      const fetchWeatherData = (city) => {
+        console.log(`Fetching weather data for ${city}`);
+      };
+      
 
   return (
     <div className="Chat">
-      {/* Render messages here */}
-      <UserInput />
+      {messages.map((message, index) => (
+        <Message key={index} type={message.type} text={message.text} />
+        ))}
+
+        <UserInput onSubmit={(input) => handleUserInput(input)} />
+
     </div>
   );
 }
