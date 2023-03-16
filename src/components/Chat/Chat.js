7733865e1d692c664e2fc48c3ac1e8cd623dd72d
Tae Kim <taekim.dev@gmail.com>
2023-03-17 03:01:50 +0800

    Added to fetch weather data

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Message from '../Message/Message';
 import UserInput from '../UserInput/UserInput';
 import './Chat.css';
@@ -19,9 +20,37 @@ function Chat() {
         fetchWeatherData(city);
       };
       
-      const fetchWeatherData = (city) => {
-        console.log(`Fetching weather data for ${city}`);
-      };
+      const fetchWeatherData = async (city) => {
+        try {
+          const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+          console.log(url)
+      
+          const response = await axios.get(url);
+          const data = response.data;
+      
+          const weatherInfo = {
+            description: data.weather[0].description,
+            temperature: data.main.temp.toFixed(1),
+            feelsLike: data.main.feels_like.toFixed(1),
+            humidity: data.main.humidity,
+          };
+      
+          const weatherMessage = `Current weather in ${city}:
+            - Description: ${weatherInfo.description}
+            - Temperature: ${weatherInfo.temperature}°C
+            - Feels like: ${weatherInfo.feelsLike}°C
+            - Humidity: ${weatherInfo.humidity}%`;
+      
+          setMessages([...messages, { type: 'bot', text: weatherMessage }]);
+        } catch (error) {
+          console.error(error);
+          setMessages([
+            ...messages,
+            { type: 'bot', text: `An error occurred while fetching the weather data for ${city}. Please try again.` },
+          ]);
+        }
+      };      
       
 
   return (
